b671b7bd3724d8aceb22f9d410d7189d8aa2f7f6
FlarisGuan <flaris.guan@gmail.com>
2014-12-03 11:10:31 +0800

    Add visualization module.

---
+++ dependencies/visualization.cpp
@@ -0,0 +1,52 @@
+#include "visualization.h"
+
+#include <pcl/visualization/pcl_visualizer.h>
+#include <string>
+#include <cstdio>
+#include <cmath>
+
+int Visualization::showMultiClouds(std::vector<CloudPtr> &clouds, int viewPorts)
+{
+    if (viewPorts < 1)
+        return -2;
+
+    pcl::visualization::PCLVisualizer viewer("Viewer");
+
+    int width = (int)sqrt((double)viewPorts);
+    int height = (int)(viewPorts / width);
+    if (width * height < viewPorts) height++;
+
+    int *portRef = new int[viewPorts];
+    for (int i = 0; i < viewPorts; i++)
+    {
+        portRef[i] = i;
+        viewer.createViewPort((1.0 / width) * (i % width),
+                              (1.0 / height) * (i / height),
+                              (1.0 / width) * (i % width + 1),
+                              (1.0 / height) * (i / height + 1),
+                              portRef[i]);
+    }
+
+    viewer.initCameraParameters();
+    std::string str_buf;
+    char count_buf[10];
+
+    for (int i = 0; i < clouds.size(); i++)
+    {
+        str_buf.clear();
+        str_buf.push_back('p');
+        sprintf_s(count_buf, "%d", i);
+        str_buf.append(count_buf);
+
+        viewer.addPointCloud(clouds[i], str_buf, i % viewPorts);
+    }
+
+    viewer.setShowFPS(false);
+    viewer.setCameraPosition(-1, 1, -10, 0, -1, 0);
+    viewer.setPosition(250, 100);
+    viewer.setSize(800, 600);
+    viewer.spin();
+
+    delete[] portRef;
+    return 0;
+}
---
+++ dependencies/visualization.h
@@ -0,0 +1,13 @@
+#ifndef _VISUALIZATION_H_
+#define _VISUALIZATION_H_
+
+#include "typedef.h"
+#include <vector>
+
+class Visualization
+{
+public:
+    static int showMultiClouds(std::vector<CloudPtr> &clouds, int viewPorts = 1);
+};
+
+#endif
